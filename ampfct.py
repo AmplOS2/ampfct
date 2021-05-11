8a0c0f536966165a9fa8aa39c5c98501ffb1c844
chris <chrissx@chrissx.de>
2021-05-12 01:15:28 +0200

    i actually managed to do something finally and it makes sense

---
+++ ampfct.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+from PIL import Image
+from bitarray import bitarray
+
+bits = bitarray(endian='big')
+
+i = Image.open(open('term_font.png', 'rb'))
+for r in range(16):
+    for c in range(16):
+        for y in range(r*11, (r+1)*11):
+            for x in range(c*8, (c+1)*8):
+                bits.append(1 if i.getpixel((x, y)) != 0 else 0)
+
+open('term_font.bin', 'wb').write(bits.tobytes())
